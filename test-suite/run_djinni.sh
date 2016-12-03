cc2fe6fc5d64683221a072e17d9913281875efdb
Jamie Houston <foresterh@gmail.com>
2016-12-02 17:33:16 -0800

    updating test to run cx

--- test-suite/run_djinni.sh
+++ test-suite/run_djinni.sh
@@ -28,6 +28,7 @@ wchar_in_relative="djinni/wchar_test.djinni"
 temp_out_relative="djinni-output-temp"
 
 cpp_out="$base_dir/generated-src/cpp"
+cx_out="$base_dir/generated-src/cx"
 jni_out="$base_dir/generated-src/jni"
 objc_out="$base_dir/generated-src/objc"
 java_out="$base_dir/generated-src/java/com/dropbox/djinni/test"
@@ -87,6 +88,9 @@ fi
     --objcpp-out "$temp_out_relative/objc" \
     --objc-type-prefix DB \
     \
+    --cx-out $cx_out \
+    --cx-namespace testsuite \
+    \
     --yaml-out "$temp_out_relative/yaml" \
     --yaml-out-file "yaml-test.yaml" \
     --yaml-prefix "test_" \
@@ -114,6 +118,9 @@ fi
     --objc-out "$temp_out_relative/objc" \
     --objcpp-out "$temp_out_relative/objc" \
     --objc-type-prefix DB \
+    \
+    --cx-out $cx_out \
+    --cx-namespace testsuite \
     \
     --list-in-files "./generated-src/inFileList.txt" \
     --list-out-files "./generated-src/outFileList.txt"\
@@ -145,6 +152,9 @@ cp "$base_dir/djinni/yaml-test.djinni" "$temp_out/yaml"
     --objc-out "$temp_out_relative/objc" \
     --objcpp-out "$temp_out_relative/objc" \
     --objc-type-prefix DB \
+    \
+    --cx-out $cx_out \
+    --cx-namespace testsuite \
     \
     --idl "$temp_out_relative/yaml/yaml-test.djinni" \
 )
